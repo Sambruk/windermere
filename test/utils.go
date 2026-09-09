1f87bd62affa84a1c0577988132168786018fcd5
Aitor Iturri <aitor.iturri@teachiq.com>
2026-09-09 15:01:23 +0200

    fix(tests): fail eager on error
    Fail as soon as we get an error to avoid focusing on errors that depend
    on previous errors.
    
    Also keep the line number when failing in the helper function.

--- test/utils.go
+++ test/utils.go
@@ -3,12 +3,15 @@ package test
 import "testing"
 
 func Ensure(t *testing.T, err error) {
+	t.Helper()
 	if err != nil {
 		t.Errorf("%v", err)
+		t.FailNow()
 	}
 }
 
 func MustFail(t *testing.T, err error) {
+	t.Helper()
 	if err == nil {
 		t.Errorf("expected error")
 	}
